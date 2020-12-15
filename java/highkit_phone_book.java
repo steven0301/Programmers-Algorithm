e0f2027dc84b5ec63babda23f8f2ef7ed24be490
Youngjin Kim <steven.kimyjin@lgcns.com>
2020-12-16 01:10:50 +0900

    update java code

---
+++ java/highkit_phone_book.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        for(int i=0; i<phone_book.length-1 ;i++){
+            for(int j=i+1; j<phone_book.length ;j++){
+                if(phone_book[i].startsWith(phone_book[j]) || 
+                   phone_book[j].startsWith(phone_book[i])){
+                    return false;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
